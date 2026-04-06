2af954ed3746c98c80246281be5799fdbdb33f6d
Nikolas-Bars <kasp2409@mail.ru>
2026-04-06 12:09:25 +0300

    back

--- chat-back/src/app.module.ts
+++ chat-back/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,7 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(getTypeOrmModuleOptions()),
     UsersModule,
     AuthModule,
