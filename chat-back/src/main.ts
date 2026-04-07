dbb8c04e0726b5353f1145ae1cf6317e31bfa162
Nikolas-Bars <kasp2409@mail.ru>
2026-04-07 10:51:02 +0300

    added prefix

--- chat-back/src/main.ts
+++ chat-back/src/main.ts
@@ -24,6 +24,7 @@ function getCorsOrigins(): string[] {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
     origin: getCorsOrigins(),
